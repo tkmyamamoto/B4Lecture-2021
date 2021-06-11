324b43d0781e2a855cd663d8d90ee07cc366e947
sehun_kim <kim.sehun@g.sp.m.is.nagoya-u.ac.jp>
2021-06-11 16:33:29 +0900

    added function for animating each iteration

--- ex_7/sehunkim/main.py
+++ ex_7/sehunkim/main.py
@@ -3,17 +3,24 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
+import os
+import imageio
 from scipy.stats import multivariate_normal
 import argparse
 import csv
 
 
-def plot1d(data, K, pi, mu, sigma, likelihood):
+def plot1d(data, K, pi, mu, sigma, iter):
     """
-    Function for plotting 2D data
+    Function for plotting 1D data
     --------------------
     Parameters  :
-    data        : Data array to plot
+    data        : Original data array to plot
+    K           : Number of clusters
+    pi          : Probability of selecting k th gaussian
+    mu          : Mean of each gaussian distribution
+    sigma       : Variance of each gaussian distribution
+    iter        : Current iteration
     """
     x = np.linspace(np.min(data), np.max(data), 100)
     y = np.zeros(100)
@@ -26,17 +33,23 @@ def plot1d(data, K, pi, mu, sigma, likelihood):
 
     plt.xlabel('x')
     plt.ylabel('probability density')
-    plt.legend()
-    plt.title('1D GMM (K = %d)' % (K))
-    plt.savefig("result_1d.png")
+    plt.legend(loc='upper right')
+    plt.title('1D GMM (K = %d), Iter = %d' % (K, iter))
+    plt.savefig("result_iter=%d.png" % (iter))
+    plt.clf()
 
 
-def plot2d(data, K, pi, mu, sigma, likelihood):
+def plot2d_3D(data, K, pi, mu, sigma, iter, fig):
     """
     Function for plotting 2D data
     --------------------
     Parameters  :
-    data        : Data array to plot
+    data        : Original data array to plot
+    K           : Number of clusters
+    pi          : Probability of selecting k th gaussian
+    mu          : Mean of each gaussian distribution
+    sigma       : Variance of each gaussian distribution
+    iter        : Current iteration
     """
     data_x, data_y = data.T
     mu_x, mu_y = mu.T
@@ -49,9 +62,8 @@ def plot2d(data, K, pi, mu, sigma, likelihood):
     for i in range(K):
         Z += pi[i] * multivariate_normal.pdf(XY, mu[i], sigma[i])
 
-    fig = plt.figure()
     ax = Axes3D(fig)
-
+    ax.set_title("2D GMM (K = %d), iter = %d" % (K, iter), size=20)
     ax.scatter3D(data_x, data_y, np.zeros_like(
         data_x), s=2, label='original data')
     ax.contour3D(X, Y, Z, 50)
@@ -61,12 +73,44 @@ def plot2d(data, K, pi, mu, sigma, likelihood):
     ax.set_ylabel('y')
     ax.set_zlabel('probability density')
 
-    plt.legend()
+    ax.legend()
+    ax.view_init(azim=70, elev=40)
+    fig.savefig("result_iter=%d.png" % (iter))
+    plt.clf()
+
+
+def rotate_3D():
+    """
+    Function for ganerating rotating 3D plot
+    """
+    fig = plt.figure()
+    ax = Axes3D(fig)
+
     def rotate(angle):
-        ax.view_init(azim=angle)
+        ax.view_init(azim=angle, elev=40)
     rot_animation = animation.FuncAnimation(
         fig, rotate, frames=100, interval=50)
     rot_animation.save('rotation3D.gif', dpi=80)
+
+
+def make_gif(iter):
+    """
+    Function for combining every .png file into .gif file and erase .png files
+    """
+    filenames = []
+    for i in range(iter):
+        filename = f'result_iter={i}.png'
+        filenames.append(filename)
+
+    # build gif
+    with imageio.get_writer('moving_iter.gif', mode='I') as writer:
+        for filename in filenames:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+    # Remove files
+    for filename in set(filenames):
+        os.remove(filename)
 
 
 def gauss(x, mean, cov):
@@ -100,9 +144,8 @@ def em_algo(x, K, D, mu, sigma, pi):
     return pi, mu, sigma, q
 
 
-def gmm_classification(x, K, D):
+def gmm_classification(x, K, D, max_iter):
     # Initialize each variable
-    max_iter = 20
     N = len(x)
     mu = np.linspace(np.min(x), np.max(x), K)
     sigma = np.zeros((K, D, D))
@@ -117,14 +160,19 @@ def gmm_classification(x, K, D):
     old_likelihood = 0
     new_likelihood = 1
     likelihood = []
-
+    fig = plt.figure()
     for i in range(max_iter):
-        if abs(old_likelihood - new_likelihood) < 0.1:
+        if abs(old_likelihood - new_likelihood) < 0.01:
             break
         old_likelihood = new_likelihood
         pi, mu, sigma, q = em_algo(x, K, D, mu, sigma, pi)
         new_likelihood = np.sum(np.log(np.sum(q/(2*np.pi) ** (D/2), axis=1)))
         likelihood.append(new_likelihood)
+        if D == 1:
+            plot1d(x, K, pi, mu, sigma, i)
+        elif D == 2:
+            plot2d_3D(x, K, pi, mu, sigma, i, fig)
+    make_gif(i)
 
     return pi, mu, sigma, likelihood
 
@@ -135,17 +183,14 @@ def main():
     parser.add_argument("-f", dest="filename", help='Filename', required=True)
     parser.add_argument("-k", dest="K", type=int,
                         help='Number of clusters', required=False, default=3)
+    parser.add_argument("-i", dest="max_iter", type=int,
+                        help='Number of clusters', required=False, default=50)
     args = parser.parse_args()
     data = pd.read_csv(args.filename, header=None).values
     dim = np.shape(data)[1]
     K = args.K
-    pi, mu, sigma, likelihood = gmm_classification(data, K, dim)
-
-    if dim == 1:
-        plot1d(data, K, pi, mu, sigma, likelihood)
-
-    elif dim == 2:
-        plot2d(data, K, pi, mu, sigma, likelihood)
+    max_iter = args.max_iter
+    pi, mu, sigma, likelihood = gmm_classification(data, K, dim, max_iter)
 
 
 if __name__ == "__main__":
